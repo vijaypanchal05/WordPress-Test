3d5e3fa2bac7a2afdfb607e55407c3010331ef9b
vijaypanchal05 <36065019+vijaypanchal05@users.noreply.github.com>
2018-03-31 15:09:39 +0530

    code changes.
    code changes final.

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
  *
- * @package WordPress
- */
+ * @package WordPress 
+ */ 
 
 /**
- * Tells WordPress to load the WordPress theme and output it.
+ * Tells WordPress to load the WordPress theme and output it display. 
  *
  * @var bool
  */
